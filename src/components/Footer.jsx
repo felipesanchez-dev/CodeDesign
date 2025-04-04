761107e988a07b788b31d2460d90f50bad90a665
felipesanchez-dev <jfelipe9.121@gmail.com>
2025-04-04 12:29:48 -0500

    Actualiza los componentes Contact y Footer para indicar que están "pronto disponibles"

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,7 @@
 const Footer = () => {
   return (
     <footer className="w-screen bg-[#5542ff] py-4 text-black">
-      <h1>Footer</h1>
+      <h1>Footer (pronto disponible)</h1>
     </footer>
   );
 };
